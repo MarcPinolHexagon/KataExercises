79be3ff062a176932469e942e0d4b9b079db04de
Marc Pinol <marc.pinol@hexagon.com>
2025-02-25 19:08:42 +0100

    Renamed solution and added KataRomanNumeralsHelper

---
+++ KataRomanNumeralsHelper.Tests/SolutionTest.cs
@@ -0,0 +1,52 @@
+﻿namespace KataRomanNumeralsHelper.Tests
+{
+    // TODO: Replace examples and use TDD by writing your own tests
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test, Order(1)]
+        public void TestToRoman_001()
+        {
+            int input = 1;
+            string expected = "I";
+
+            string actual = RomanNumerals.ToRoman(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test, Order(2)]
+        public void TestToRoman_002()
+        {
+            int input = 2;
+            string expected = "II";
+
+            string actual = RomanNumerals.ToRoman(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test, Order(3)]
+        public void TestFromRoman_001()
+        {
+            string input = "I";
+            int expected = 1;
+
+            int actual = RomanNumerals.FromRoman(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test, Order(4)]
+        public void TestFromRoman_002()
+        {
+            string input = "II";
+            int expected = 2;
+
+            int actual = RomanNumerals.FromRoman(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ KataRomanNumeralsHelper/RomanNumerals.cs
@@ -0,0 +1,15 @@
+﻿namespace KataRomanNumeralsHelper
+{
+    public class RomanNumerals
+    {
+        public static string ToRoman(int n)
+        {
+            return "";
+        }
+
+        public static int FromRoman(string romanNumeral)
+        {
+            return 0;
+        }
+    }
+}
