407e163e2dbb8fcb6fca862cacf1f299c07eeeff
Marc Pinol <marc.pinol@hexagon.com>
2025-02-25 19:25:49 +0100

    Added KataEventAndDelegate

---
+++ KataEventAndDelegate.Tests/SolutionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace KataEventAndDelegate.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void BasicTest()
+        {
+            List<string> peopleList = new List<string>()
+            {
+                "Peter", "Mike", "Peter", "Bob", "Peter", "Peter", "Bob", "Mike", "Bob", "Peter", "Peter", "Mike", "Bob"
+            };
+            TextMessageSend.TextMessageList = "";
+            Publisher publisher = new Publisher();
+            publisher.ContactNotify += TextMessageSend.Send;
+            publisher.CountMessages(peopleList);
+            string output = TextMessageSend.TextMessageList;
+            string expected = "Peter, Bob, Peter, Mike";
+            Assert.That(output, Is.EqualTo(expected).IgnoreCase);
+        }
+    }
+
+    public class TextMessageSend
+    {
+        public static string TextMessageList { get; set; } = string.Empty;
+
+        public static void Send(object source, PersonEventArgs e)
+        {
+            TextMessageList.Append<>($", {e.Name}");
+        }
+    }
+}
---
+++ KataEventAndDelegate/EventAndDelegate.cs
@@ -0,0 +1,22 @@
+﻿namespace KataEventAndDelegate
+{
+    public class PersonEventArgs : EventArgs
+    {
+        // implement custom event handler with Name property (string)
+    }
+
+    public class Publisher
+    {
+        public event /*delegate*/ ContactNotify;
+
+
+        public void CountMessages(List<string> peopleList)
+        {
+            foreach (string person in peopleList)
+            {
+                // implement your logic to send name of people after appearing 3*n times
+                OnContactNotify(person); // Notify subscribers
+            }
+        }
+    }
+}
